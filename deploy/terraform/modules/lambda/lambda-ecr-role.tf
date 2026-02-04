33e9f9c0c5fcb86f26d6243a23b7e27f4487e991
Matheus Ribeiro <teteuoliveira12@gmail.com>
2026-02-03 21:29:02 -0300

    feat(setup-lambda-auth): remover MS_TOKEN_SECRET da configuração do Terraform
    - Removido o parâmetro MS_TOKEN_SECRET do mapa de variáveis em auth_lambda.tf.
    
    Essa alteração simplifica a configuração do Terraform para a autenticação do AWS Lambda, eliminando uma variável que não é mais necessária. Isso melhora a clareza e a manutenção do código, garantindo que apenas as variáveis relevantes sejam mantidas na configuração.

--- deploy/terraform/modules/lambda/lambda-ecr-role.tf
+++ deploy/terraform/modules/lambda/lambda-ecr-role.tf
@@ -21,6 +21,48 @@ data "aws_iam_policy_document" "ecr_lambda_access_policy" {
   }
 }
 
+data "aws_iam_policy_document" "ecr_allow_lambda" {
+  statement {
+    sid    = "AllowLambdaPull"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer"
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "allow_lambda" {
+  repository = aws_ecr_repository.lambda_repository.name
+  policy     = data.aws_iam_policy_document.ecr_allow_lambda.json
+}
+
+data "aws_iam_policy_document" "lambda_ecr_access" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchCheckLayerAvailability"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_ecr_access" {
+  role   = aws_iam_role.lambda_exec_role.id
+  policy = data.aws_iam_policy_document.lambda_ecr_access.json
+}
+
 resource "aws_ecr_repository_policy" "allow_lambda_to_access_ecr" {
   repository = aws_ecr_repository.lambda_repository.name
   policy     = data.aws_iam_policy_document.ecr_lambda_access_policy.json
